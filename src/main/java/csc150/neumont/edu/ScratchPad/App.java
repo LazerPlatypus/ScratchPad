d74cfaec13876d8ee5901bac67ea3b429bf3c332
Adam Holt (Laptop) <aholt@student.neumont.edu>
2019-01-07 16:45:17 -0700

    First commit, added HoltALibrary

---
+++ src/main/java/csc150/neumont/edu/ScratchPad/App.java
@@ -0,0 +1,13 @@
+package csc150.neumont.edu.ScratchPad;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
